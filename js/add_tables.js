7057fb647df1904b68e7869081094b76892ed475
joellegg <joellegg@bellsouth.net>
2017-03-10 10:34:39 -0600

    added errorhandler and FOREIGN KEY REFERENCES

--- js/add_tables.js
+++ js/add_tables.js
@@ -6,10 +6,41 @@ const { Database } = require('sqlite3').verbose();
 // Database method accepts a callback function for successful connection
 const db = new Database('bangazon.sqlite', () => console.log('Tables added!'));
 
+// errorHandler is a function which accepts an error object
+const errorHandler = (err) => {
+  if (err) { // If there is an error obj, it will be console logged
+    console.log(`Msg: ${err}`);
+  };
+};
+
 // Passing in IF NOT EXISTS after CREATE TABLE will check to make sure there are no tables named 'customers'
 // If it does exist, this line will not run
-db.run("CREATE TABLE IF NOT EXISTS Customers (customer_id INTEGER PRIMARY KEY, name TEXT, address TEXT, city TEXT, state TEXT, postal_code INT, phone_number INT)");
-db.run("CREATE TABLE IF NOT EXISTS Payment_Options (payment_id INTEGER PRIMARY KEY, name TEXT, account_number INT)");
-db.run("CREATE TABLE IF NOT EXISTS Products (product_id INTEGER PRIMARY KEY, name TEXT, price INT)");
-db.run("CREATE TABLE IF NOT EXISTS Orders (order_id INTEGER PRIMARY KEY, customer_id INT, payment_id INT, payment_status INT)");
-db.run("CREATE TABLE IF NOT EXISTS Order_Line (line_id INTEGER PRIMARY KEY, order_id INT, product_id INT)");
+db.run(`CREATE TABLE IF NOT EXISTS Customers (
+  customer_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  address TEXT,
+  city TEXT,
+  state TEXT,
+  postal_code INT,
+  phone_number INT)`, errorHandler);
+db.run(`CREATE TABLE IF NOT EXISTS Payment_Options (
+  payment_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  account_number INT)`, errorHandler);
+db.run(`CREATE TABLE IF NOT EXISTS Products (
+  product_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  price INT)`, errorHandler);
+db.run(`CREATE TABLE IF NOT EXISTS Orders (
+  order_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  customer_id INT,
+  payment_id INT,
+  payment_status INT,
+  FOREIGN KEY(customer_id) REFERENCES Customers(customer_id),
+  FOREIGN KEY(payment_id) REFERENCES Payment_Options(payment_id))`, errorHandler);
+db.run(`CREATE TABLE IF NOT EXISTS Order_Line (
+  line_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  order_id INT,
+  product_id INT,
+  FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY(product_id) REFERENCES Products(product_id))`, errorHandler);
